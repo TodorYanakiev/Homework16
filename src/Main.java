09534bae731a4e21703b17948dde306c43c6f0a3
TodorYanakiev <todoryanakiev395@gmail.com>
2023-06-09 22:32:15 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static int textLength(){
+        Scanner sc = new Scanner(System.in);
+        String text = sc.nextLine();
+        String[] words = text.split("[ ,.,,]");
+        int result = words.length;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals("")) result--;
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        //ex1
+//        System.out.println(textLength());
     }
 }
